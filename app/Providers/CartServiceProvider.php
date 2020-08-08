15d7c3d27950996c2828768ded36ce9972a93003
Ardhiyatma Raditya Wibawa <ardiyatma.wibawa12@gmail.com>
2020-08-08 12:20:30 +0700

    Cara membuat facade di laravel

---
+++ app/Providers/CartServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CartServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        App::bind('cart',function()
+        {
+            return new Cart();
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
